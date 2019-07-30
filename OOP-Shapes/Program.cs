5567c7e2326aa5669c346e025d1b37e3de168c51
max-student@outlook.com <max-student@outlook.com>
2019-07-30 12:04:46 -0400

    Add project files.

---
+++ OOP-Shapes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace OOP_Shapes {
+    class Program {
+        static void Main(string[] args) {
+
+            var quad1 = new Quad(5, 7, 13, 6);
+
+            var perimeter = quad1.GetPerimeter();
+
+            Console.WriteLine($"The perimeter is {perimeter}");
+        }
+    }
+}
---
+++ OOP-Shapes/Quad.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Shapes {
+    public class Quad {
+        public int side1 { get; set; }
+        public int side2 { get; set; }
+        public int side3 { get; set; }
+        public int side4 { get; set; }
+
+        public int GetPerimeter() {
+            return side1 + side2 + side3 + side4;
+        }
+
+        public Quad(int side_a, int side_b, int side_c, int side_d) {
+            this.side1 = side_a;
+            this.side2 = side_b;
+            this.side3 = side_c;
+            this.side4 = side_d;
+        }
+    }
+}
